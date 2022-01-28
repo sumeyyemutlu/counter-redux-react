5fedb6b565c4e5927a5f5193296e4a4cd012eb86
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-01-28 08:56:02 +0300

    index.js dosyası düzenlendi

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,6 +2,6 @@ import  {combineReducers} from "redux";
 import counterReducer from "./counterReducers";
 
 const reducers = combineReducers({ //reducerları birleştir metodu
-    combineReducers // bu aslısrcnda combineReducers:combineReducers demek aynı isim olduğu içn böyle verdik. 
+    counterReducer// bu aslında counterReducer:counterReducer demek aynı isim olduğu içn böyle verdik. 
 })
 export default reducers;
