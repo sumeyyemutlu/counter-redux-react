06c9db888b04481c88be2fd766a2a6ce32042a84
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-01-28 08:42:50 +0300

    actiona göre state tanımlaması

---
+++ src/redux/reducers/counterReducers.js
@@ -0,0 +1,16 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const counterReducer = (state=0, action) =>{//state tanımsız olmaması için ilk değerini verdik
+    let newState ;//arrayin bir kopyasını, referansını oluşturduk onun üstünde işlem yapacağız
+    switch(action.type) {
+        case actionTypes.INCREASE_COUNTER:
+            return (newState = state + action.payload);
+        case actionTypes.DECREASE_COUNTER:
+            return (newState = state - action.payload);
+        case actionTypes.INCREASE_BY_TWO_COUNTER:
+            return (newState = state + action.payload);
+        default:
+            return state;
+    }
+}
+export default counterReducer;
