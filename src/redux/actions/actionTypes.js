a58daa471029a5faeb289b8a3450e144467c92b8
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-01-28 08:39:18 +0300

    actions dosyaları oluşturuldu

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,6 @@
+export const INCREASE_COUNTER ="INCREASE_COUNTER";
+export const DECREASE_COUNTER ="DECREASE_COUNTER";
+export const INCREASE_BY_TWO_COUNTER ="INCREASE_BY_TWO_COUNTER ";
+//aksiyonlarımızın isimlerini tuttuğumuz dosya.
+//Redux ile ilk iletişim kurudğumuz yer.
+//Type’lar genelde değişmeyen, sadece yönlendirici sabitler olduğu için ayrı bir dosyada tutulur(actionTypes).
---
+++ src/redux/actions/counterActions.js
@@ -0,0 +1,17 @@
+import * as actionTypes from './actionTypes';
+
+//action creator bir arrow fonksiyondur. değiştiği için payload kısmını parametre alır ve bir action döner.
+export const increaseCounter = () => ({
+    type:actionTypes.INCREASE_COUNTER,//actionTypes birer yönlendirici sabittir.
+    payload: 1
+})
+
+export const decreaseCounter = () => ({
+    type:actionTypes.DECREASE_COUNTER,
+    payload: 1
+})
+
+export const increaseByTwoCounter = () => ({
+    type:actionTypes.INCREASE_BY_TWO_COUNTER,
+    payload: 2
+})
